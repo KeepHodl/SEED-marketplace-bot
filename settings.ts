ba853928b55a03ddbd08bc6dbdab8270de05bfdd
KeepHodl <konstshcht@gmail.com>
2024-07-14 15:25:18 +0500

    fix

--- settings.ts
+++ settings.ts
@@ -1,21 +1,21 @@
 // Price for buying EGG lower or equal to numbers
 export const BUY_PRICES = {
-  common: 18,
-  uncommon: 35,
-  rare: 100,
-  epic: 400,
+  common: 22,
+  uncommon: 65,
+  rare: 280,
+  epic: 1200,
   legendary: 5000,
 };
 
 // Price for selling EGG. If it's 0, it will not be sold
 export const SELL_PRICES = {
-  common: 23,
-  uncommon: 54,
-  rare: 0,
+  common: 27,
+  uncommon: 78,
+  rare: 324,
   epic: 0,
   legendary: 0,
 };
 
 // Your telegram data. You can get it from the browser console
 export const TELEGRAM_DATA =
-  'user=...blyyyyt';
+  'user=';
